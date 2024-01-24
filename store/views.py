3b44ec7cb1012c76a5461f72f8745ba6c82e0adf
dfamiliarstranger <eniolaemmanuel@gmail.com>
2024-01-24 02:23:04 +0100

    cart.js

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 def store(request):
@@ -30,3 +32,18 @@ def checkout(request):
     
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
+
+def UpdateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    Customer = request.user.customer
+    product = Product.object.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    OrderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    return JsonResponse('ITEM ADDED', safe=False)
